3fe14581160eacccfec9d389c6e3994687f162bc
Gagan jain004 <gagan.praveen004@gmail.com>
2025-12-12 22:56:30 +0530

    Unwanted removed

--- data/faqs.js
+++ data/faqs.js
@@ -9,11 +9,11 @@ export const faqs = [
     answer:
       "Career Ai learns about your industry, experience, and skills during onboarding. It then uses this information to generate customized resumes, cover letters, and interview questions. The content is specifically aligned with your professional background and industry standards, making it highly relevant and effective.",
   },
-  {
-    question: "How accurate and up-to-date are Career Ai's industry insights?",
-    answer:
-      "We update our industry insights weekly using advanced AI analysis of current market trends. This includes salary data, in-demand skills, and industry growth patterns. Our system constantly evolves to ensure you have the most relevant information for your career decisions.",
-  },
+  // {
+  //   question: "How accurate and up-to-date are Career Ai's industry insights?",
+  //   answer:
+  //     "We update our industry insights weekly using advanced AI analysis of current market trends. This includes salary data, in-demand skills, and industry growth patterns. Our system constantly evolves to ensure you have the most relevant information for your career decisions.",
+  // },
   {
     question: "Is my data secure with Career Ai?",
     answer:
